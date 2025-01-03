20a502ef674d91d39ca66da375186087cdfedbde
danaklv <kalykovadana3@gmail.com>
2025-01-03 21:54:43 +0600

    log/reg form change

---
+++ todo/handle/PasswordHandler.go
@@ -0,0 +1,7 @@
+package handle
+
+import "net/http"
+
+func PasswordHandler(w http.ResponseWriter, r *http.Request) {
+	
+}
--- todo/handle/handlers.go
+++ todo/handle/handlers.go
@@ -9,6 +9,6 @@ func Handlers() {
 	http.HandleFunc("/", MainPageHandler)
 	http.HandleFunc("/login", LoginPageHandler)
 	http.HandleFunc("/register", RegisterPageHandler)
-	
+	http.HandleFunc("/forgotPassword", PasswordHandler)	
 	
 }
